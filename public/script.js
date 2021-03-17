25752d9538444f452c7486babad571d4ee2c424b
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-16 20:40:58 -0700

    Adding our video but muting it

--- public/script.js
+++ public/script.js
@@ -1,8 +1,11 @@
 const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
 const mypeer = new peer(undefined, {
     host: '/',
     port: 3001
 })
+const myVideo = document.createElement('video')
+myVideo.muted = true
 
 mypeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, 10)
