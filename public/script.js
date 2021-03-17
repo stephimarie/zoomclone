0dd6887458a6397fcadea2749876a8f9b5cd1b92
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-16 21:22:28 -0700

    Added our video to call

--- public/script.js
+++ public/script.js
@@ -19,6 +19,10 @@ navigator.mediaDevices.getUserMedia({
 
     myPeer.on('call', call => {
         call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
     })
 
     socket.on('user-connected', userId =>{
