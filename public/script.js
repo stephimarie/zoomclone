3081a19ecfc895d4696f3d030cf13fdb4136d235
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-16 20:45:03 -0700

    Adding our video and audio

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
+const { truncate } = require("fs")
+const { Stream } = require("stream")
+
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const mypeer = new peer(undefined, {
@@ -6,6 +9,13 @@ const mypeer = new peer(undefined, {
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(Stream => {
+    
+})
 
 mypeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, 10)
