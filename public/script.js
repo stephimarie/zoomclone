b9e37e1aa328eab6b52e1cd2f8181cb4a8e7d929
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-18 19:48:26 -0700

    Add common

--- public/script.js
+++ public/script.js
@@ -32,25 +32,25 @@ socket.on('user-disconnected', userId => {
 
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id)
-})
+});
 
 function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream)
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
-  })
+  });
   call.on('close', () => {
     video.remove()
-  })
+  });
 
   peers[userId] = call
-}
+};
 
 function addVideoStream(video, stream) {
   video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
     video.play()
-  })
+  });
   videoGrid.append(video)
-}
+};
