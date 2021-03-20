7917c33c9e881e262048bbeff163c24880b3e45c
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-20 12:09:32 -0700

    remove broadcast

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
+const rooms = io.of("/").adapter.rooms;
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -18,12 +19,21 @@ app.get('/:room', (req, res) => {
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    console.log("userId", userId)
+    socket.to(roomId).emit('user-connected', userId)
 
     socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      socket.to(roomId).emit('user-disconnected', userId)
     });
   });
 });
 
-server.listen(3000)
+// io.of("/").adapter.on("create-room", (room) => {
+//   console.log(`room ${room} was created`);
+// });
+
+// io.of("/").adapter.on("join-room", (room, id) => {
+//   console.log(`socket ${id} has joined room ${room}`);
+// });
+
+server.listen(5000)
